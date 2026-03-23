4a455393464dd4da636fd51b775d9d6b9c859162
AnkitaPatil <patilankita18751@gmail.com>
2026-03-23 16:27:20 +0530

    Adding C# Assignments

---
+++ Assignments/C#LINQAssignment9/Exercise1.cs
@@ -0,0 +1,43 @@
+﻿namespace C_LINQAssignment9
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+        public int Marks;
+    }
+
+    internal class Exercise1
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>
+        {
+            new Student{Id=1, Name="Amit", Age=20, Marks=80},
+            new Student{Id=2, Name="Ravi", Age=22, Marks=70},
+            new Student{Id=3, Name="Kiran", Age=18, Marks=90},
+            new Student{Id=4, Name="Anil", Age=25, Marks=60}
+        };
+
+            // 1. Marks > 75
+            var highMarks = students.Where(s => s.Marks > 75);
+
+            // 2. Age between 18 and 25
+            var ageFilter = students.Where(s => s.Age >= 18 && s.Age <= 25);
+
+            // 3. Sort by Marks descending
+            var sorted = students.OrderByDescending(s => s.Marks);
+
+            // 4. Select Name and Marks
+            var selected = students.Select(s => new { s.Name, s.Marks });
+
+            foreach (var s in selected)
+                Console.WriteLine($"{s.Name} - {s.Marks}");
+        }
+    }
+}
---
+++ Assignments/C#LINQAssignment9/Exercise10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C_LINQAssignment9
+{
+    public record Employee1(int Id, string Name, string Department, double Salary);
+    internal class Exercise10
+    {
+            static void Main()
+            {
+                var employees = new List<Employee1>()
+            {
+                new Employee1(1,"A","IT",50000),
+                new Employee1(2,"B","HR",40000),
+                new Employee1(3,"C","IT",70000)
+            };
+
+                var result = from e in employees
+                             orderby e.Department, e.Salary descending
+                             select e;
+
+                foreach (var e in result)
+                    Console.WriteLine($"{e.Department} - {e.Salary}");
+            }
+        }
+    }
