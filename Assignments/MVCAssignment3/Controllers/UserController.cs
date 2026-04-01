3b372a16ddd9c97d93c7b8eb3508e54f08cf42b9
AnkitaPatil <patilankita18751@gmail.com>
2026-04-01 16:38:58 +0530

    Adding MVC Assignment

---
+++ Assignments/MVCAssignment3/Controllers/UserController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAssignment3.Models;
+using MVCAssignment3.ViewModels;
+
+namespace MVCAssignment3.Controllers
+{
+    public class UserController : Controller
+    {
+        // Temporary in-memory storage
+        private static List<User> users = new List<User>();
+
+        // ---------------- REGISTER ----------------
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                users.Add(user);
+                return RedirectToAction("Login");
+            }
+            return View(user);
+        }
+
+        // ---------------- LOGIN ----------------
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            var user = users.FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user != null)
+            {
+                HttpContext.Session.SetString("UserEmail", user.Email);
+                return RedirectToAction("Profile");
+            }
+
+            ViewBag.Error = "Invalid credentials";
+            return View();
+        }
+
+        // ---------------- PROFILE ----------------
+
+        public IActionResult Profile()
+        {
+            var email = HttpContext.Session.GetString("UserEmail");
+
+            if (email == null)
+                return RedirectToAction("Login");
+
+            var user = users.FirstOrDefault(u => u.Email == email);
+
+            var model = new UserViewModel
+            {
+                Name = user.Name,
+                Email = user.Email
+            };
+
+            return View(model);
+        }
+
+        // ---------------- EDIT ----------------
+
+        public IActionResult Edit()
+        {
+            var email = HttpContext.Session.GetString("UserEmail");
+
+            if (email == null)
+                return RedirectToAction("Login");
+
+            var user = users.FirstOrDefault(u => u.Email == email);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User updatedUser)
+        {
+            var email = HttpContext.Session.GetString("UserEmail");
+
+            if (email == null)
+                return RedirectToAction("Login");
+
+            var user = users.FirstOrDefault(u => u.Email == email);
+
+            if (user != null)
+            {
+                user.Name = updatedUser.Name;
+                user.Email = updatedUser.Email;
+
+                HttpContext.Session.SetString("UserEmail", user.Email);
+            }
+
+            return RedirectToAction("Profile");
+        }
+
+        // ---------------- LOGOUT ----------------
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Assignments/MVCAssignment3/ViewModels/UserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCAssignment3.ViewModels
+{
+    public class UserViewModel
+    {
+        public string ?Name { get; set; }
+        public string ?Email { get; set; }
+    }
+}
