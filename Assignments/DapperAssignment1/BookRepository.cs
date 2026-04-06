d08335d0e46bc71309d91340de0f88c1506db36c
AnkitaPatil <patilankita18751@gmail.com>
2026-04-06 16:23:10 +0530

    Adding Dapper Assignment

---
+++ Assignments/DapperAssignment1/BookRepository.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DapperAssignment1
+{
+    public class BookRepository
+    {
+        private readonly string connectionString;
+
+        public BookRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        private IDbConnection Connection => new SqlConnection(connectionString);
+
+        public void AddBook(Book book)
+        {
+            using (var db = Connection)
+            {
+                string sql = @"INSERT INTO Book (Title, Price, Author, Publisher, Language, PublishDate)
+                           VALUES (@Title, @Price, @Author, @Publisher, @Language, @PublishDate)";
+                db.Execute(sql, book);
+            }
+        }
+
+        public void EditBook(Book book)
+        {
+            using (var db = Connection)
+            {
+                string sql = @"UPDATE Book 
+                           SET Title=@Title, Price=@Price, Author=@Author, Publisher=@Publisher, 
+                               Language=@Language, PublishDate=@PublishDate
+                           WHERE BookId=@BookId";
+                db.Execute(sql, book);
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            using (var db = Connection)
+            {
+                string sql = "DELETE FROM Book WHERE BookId=@Id";
+                db.Execute(sql, new { Id = id });
+            }
+        }
+
+        public Book GetBook(int id)
+        {
+            using (var db = Connection)
+            {
+                return db.QuerySingleOrDefault<Book>("SELECT * FROM Book WHERE BookId=@Id", new { Id = id });
+            }
+        }
+
+        public Book GetBook(string name)
+        {
+            using (var db = Connection)
+            {
+                return db.QuerySingleOrDefault<Book>("SELECT * FROM Book WHERE Title=@Title", new { Title = name });
+            }
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            using (var db = Connection)
+            {
+                return db.Query<Book>("SELECT * FROM Book").ToList();
+            }
+        }
+
+        public List<Book> GetAllBooksByAuthor(string author)
+        {
+            using (var db = Connection)
+            {
+                return db.Query<Book>("SELECT * FROM Book WHERE Author=@Author", new { Author = author }).ToList();
+            }
+        }
+
+        public List<Book> GetAllBooksByLanguage(string lang)
+        {
+            using (var db = Connection)
+            {
+                return db.Query<Book>("SELECT * FROM Book WHERE Language=@Language", new { Language = lang }).ToList();
+            }
+        }
+
+        public List<Book> GetAllBooksByPublisher(string publisher)
+        {
+            using (var db = Connection)
+            {
+                return db.Query<Book>("SELECT * FROM Book WHERE Publisher=@Publisher", new { Publisher = publisher }).ToList();
+            }
+        }
+    }
+}
---
+++ Assignments/DapperAssignment1/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace DapperAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = "Data Source=LAPTOP-I4FPJV3B\\SQLEXPRESS;Initial Catalog=DapperAssignment1;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+            BookRepository repo = new BookRepository(connString);
+
+            while (true)
+            {
+                Console.WriteLine("\n1. Add Book");
+                Console.WriteLine("2. Edit Book");
+                Console.WriteLine("3. Delete Book");
+                Console.WriteLine("4. Get Book by ID");
+                Console.WriteLine("5. Get Book by Name");
+                Console.WriteLine("6. List All Books");
+                Console.WriteLine("7. List Books by Author");
+                Console.WriteLine("8. List Books by Language");
+                Console.WriteLine("9. List Books by Publisher");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choose an option: ");
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        var book = new Book();
+                        Console.Write("Title: "); book.Title = Console.ReadLine();
+                        Console.Write("Price: "); book.Price = decimal.Parse(Console.ReadLine());
+                        Console.Write("Author: "); book.Author = Console.ReadLine();
+                        Console.Write("Publisher: "); book.Publisher = Console.ReadLine();
+                        Console.Write("Language: "); book.Language = Console.ReadLine();
+                        Console.Write("Publish Date (yyyy-mm-dd): "); book.PublishDate = DateTime.Parse(Console.ReadLine());
+                        repo.AddBook(book);
+                        Console.WriteLine("Book added successfully!");
+                        break;
+
+                    case "2":
+                        Console.Write("Enter Book ID to edit: ");
+                        int editId = int.Parse(Console.ReadLine());
+                        var editBook = repo.GetBook(editId);
+                        if (editBook != null)
+                        {
+                            Console.Write("New Title: "); editBook.Title = Console.ReadLine();
+                            Console.Write("New Price: "); editBook.Price = decimal.Parse(Console.ReadLine());
+                            Console.Write("New Author: "); editBook.Author = Console.ReadLine();
+                            Console.Write("New Publisher: "); editBook.Publisher = Console.ReadLine();
+                            Console.Write("New Language: "); editBook.Language = Console.ReadLine();
+                            Console.Write("New Publish Date (yyyy-mm-dd): "); editBook.PublishDate = DateTime.Parse(Console.ReadLine());
+                            repo.EditBook(editBook);
+                            Console.WriteLine("Book updated successfully!");
+                        }
+                        else Console.WriteLine("Book not found.");
+                        break;
+
+                    case "3":
+                        Console.Write("Enter Book ID to delete: ");
+                        int delId = int.Parse(Console.ReadLine());
+                        repo.DeleteBook(delId);
+                        Console.WriteLine("Book deleted successfully!");
+                        break;
+
+                    case "4":
+                        Console.Write("Enter Book ID: ");
+                        int getId = int.Parse(Console.ReadLine());
+                        var bookById = repo.GetBook(getId);
+                        if (bookById != null) Console.WriteLine($"{bookById.BookId} | {bookById.Title} | {bookById.Price} | {bookById.Author} | {bookById.Publisher} | {bookById.Language} | {bookById.PublishDate}");
+                        else Console.WriteLine("Book not found.");
+                        break;
+
+                    case "5":
+                        Console.Write("Enter Book Name: ");
+                        string name = Console.ReadLine();
+                        var bookByName = repo.GetBook(name);
+                        if (bookByName != null) Console.WriteLine($"{bookByName.BookId} | {bookByName.Title} | {bookByName.Price} | {bookByName.Author} | {bookByName.Publisher} | {bookByName.Language} | {bookByName.PublishDate}");
+                        else Console.WriteLine("Book not found.");
+                        break;
+
+                    case "6":
+                        var allBooks = repo.GetAllBooks();
+                        foreach (var b in allBooks)
+                            Console.WriteLine($"{b.BookId} | {b.Title} | {b.Price} | {b.Author} | {b.Publisher} | {b.Language} | {b.PublishDate}");
+                        break;
+
+                    case "7":
+                        Console.Write("Enter Author Name: ");
+                        string author = Console.ReadLine();
+                        var booksByAuthor = repo.GetAllBooksByAuthor(author);
+                        booksByAuthor.ForEach(b => Console.WriteLine($"{b.BookId} | {b.Title} | {b.Price} | {b.Author} | {b.Publisher} | {b.Language} | {b.PublishDate}"));
+                        break;
+
+                    case "8":
+                        Console.Write("Enter Language: ");
+                        string lang = Console.ReadLine();
+                        var booksByLang = repo.GetAllBooksByLanguage(lang);
+                        booksByLang.ForEach(b => Console.WriteLine($"{b.BookId} | {b.Title} | {b.Price} | {b.Author} | {b.Publisher} | {b.Language} | {b.PublishDate}"));
+                        break;
+
+                    case "9":
+                        Console.Write("Enter Publisher: ");
+                        string pub = Console.ReadLine();
+                        var booksByPub = repo.GetAllBooksByPublisher(pub);
+                        booksByPub.ForEach(b => Console.WriteLine($"{b.BookId} | {b.Title} | {b.Price} | {b.Author} | {b.Publisher} | {b.Language} | {b.PublishDate}"));
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+            }
+        }
+    }
+}
